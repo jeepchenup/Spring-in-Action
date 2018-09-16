5eeb3038ade19a8f80f3b29085bf469bed30d141
Steven Chen <jeepchenup@foxmail.com>
2018-09-16 18:29:31 +0800

    Chapter5 WIP

---
+++ Chapter5/src/main/java/spittr/web/HomeController.java
@@ -0,0 +1,14 @@
+package spittr.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/", method=GET)
+    public String home() {
+        return "home";
+    }
+}
