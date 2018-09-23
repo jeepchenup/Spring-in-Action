eb466c505c660086f8aff6fb9090af6fe3e819bf
Steven Chen <jeepchenup@foxmail.com>
2018-09-23 20:05:53 +0800

    Chapter6 JSP done

--- Chapter6/Jsp/src/main/java/spittr/config/WebConfig.java
+++ Chapter6/Jsp/src/main/java/spittr/config/WebConfig.java
@@ -1,13 +1,19 @@
 package spittr.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Set;
 
 @Configuration
 @EnableWebMvc
@@ -24,5 +30,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setCacheSeconds(10);
+        return messageSource;
     }
 }
--- Chapter6/Jsp/src/main/resources/test_data.sql
+++ Chapter6/Jsp/src/main/resources/test_data.sql
@@ -8,3 +8,5 @@ INSERT INTO Spittle (message, created_at) VALUES ('message_7', CURRENT_DATE);
 INSERT INTO Spittle (message, created_at) VALUES ('message_8', CURRENT_DATE);
 INSERT INTO Spittle (message, created_at) VALUES ('message_9', CURRENT_DATE);
 INSERT INTO Spittle (message, created_at) VALUES ('message_10', CURRENT_DATE);
+
+INSERT INTO Spitter (username, password, first_name, last_name, email) VALUES ('jbauer','24hours','Jack','Bauer', 'jbauer@gmail.com');
